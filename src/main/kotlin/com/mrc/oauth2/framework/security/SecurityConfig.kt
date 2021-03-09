6a0225e4fc333bdfb14f1826ab79406b1d452885
전상훈 <mrc0700@hotmail.com>
2021-03-10 01:03:57 +0900

    코틀린 OAuth 2.0 작성중

---
+++ src/main/kotlin/com/mrc/oauth2/framework/security/SecurityConfig.kt
@@ -0,0 +1,43 @@
+package com.mrc.oauth2.framework.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+    }
+
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic().realmName("OAuth Server")
+    }
+
+    @Bean
+    override fun authenticationManagerBean(): AuthenticationManager {
+        return super.authenticationManagerBean()
+    }
+
+    // TODO: For example
+    // See. https://yusuke.blog/2018/03/02/2163
+    //      https://www.harinathk.com/spring/no-passwordencoder-mapped-id-null/
+    @Suppress("deprecation")
+    @Bean
+    fun passwordEncoder(): NoOpPasswordEncoder {
+        // Spring Boot 2から平文パスワードの場合Exceptionが発生する
+        // 今回はサンプルのため、平文パスワード許可する
+        return NoOpPasswordEncoder.getInstance() as NoOpPasswordEncoder
+    }
+}
